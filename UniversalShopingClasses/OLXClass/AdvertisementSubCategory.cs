545e0716da481d0cc927210ab8dd1e2ac6ddeaf2
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-08 03:17:49 +0500

    Added New Class Advertisement category subcategory and type

---
+++ UniversalShopingClasses/OLXClass/AdvertisementSubCategory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniversalShopingClasses.OLXClass
+{
+    public class AdvertisementSubCategory : IListable
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Column(TypeName = "varchar")]
+        public string Name { get; set; }
+        [Required]
+        public virtual AdvertismentCateory AdvertismentCateory { get; set; }
+    }
+}
---
+++ UniversalShopingClasses/OLXClass/AdvertisementType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniversalShopingClasses.OLXClass
+{
+    public class AdvertisementType : IListable
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Column(TypeName = "varchar")]
+        public string Name { get; set; }
+    }
+}
