135b771b2ad9a2779c42c12b4aa8676e23c7c8cc
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-07 18:19:51 +0500

    Added NEw Class for Mobiles

---
+++ UniversalShopingClasses/MobileManagement/Mobile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UniversalShopingClasses.GeneralProductManagement;
+
+namespace UniversalShopingClasses.MobileManagement
+{
+    public class Mobile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public bool BlueTooth { get; set; }
+        public bool ThreeG { get; set; }
+        public bool FourG { get; set; }
+        public bool Wifi { get; set; }
+        public int Ram { get; set; }
+        public int FrontCam { get; set; }
+        public int BackCam { get; set; }
+        public string Color { get; set; }
+        public int ScreenSize { get; set; }
+        public int Battery { get; set; }
+        public int? Stock { get; set; }
+        public string Accessories { get; set; }
+        public DateTime Warranty { get; set; }
+        public string Features { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public virtual ProductBrand ProductBrand { get; set; }
+        public virtual ICollection<ProductImages> ProductImages { get; set; }
+
+        public Mobile()
+        {
+            ProductImages = new List<ProductImages>();
+        }
+
+    }
+}
---
+++ UniversalShopingClasses/MobileManagement/MobileHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using UniversalShopingClasses.GeneralProductManagement;
+
+namespace UniversalShopingClasses.MobileManagement
+{
+    public class MobileHandler
+    {
+        public List<Mobile> GetMobilebyId(int id)
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.Mobiles.Include(m => m.ProductImages).Include(m => m.ProductBrand)
+                        where c.Id == id
+                        select c).ToList();
+            }
+        }
+        public List<Mobile> GetMobileByBrand(ProductBrand mobileBrand)
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return
+                (from m in
+                        db.Mobiles
+                            .Include(m => m.ProductBrand)
+                            .Include(m => m.ProductImages)
+                 where m.ProductBrand.Id == mobileBrand.Id
+                 select m).ToList();
+            }
+        }
+        public ProductBrand GetBrandById(int id)
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.ProductBrands
+                        where c.Id == id
+                        select c).FirstOrDefault();
+            }
+        }
+
+        public List<Mobile> GetAllMobiles()
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.Mobiles select c).ToList();
+            }
+        }
+    }
+}
