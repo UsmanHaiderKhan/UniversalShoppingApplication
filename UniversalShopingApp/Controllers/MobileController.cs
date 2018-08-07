e65aafe7e46f9b80c95f8a4e69d3583b85a7f71f
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-07 16:45:57 +0500

    Added New Controller For Mobile Poduct

---
+++ UniversalShopingApp/Controllers/MobileController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversalShopingApp.Models;
+using UniversalShopingClasses;
+using UniversalShopingClasses.DrinksManagement;
+using UniversalShopingClasses.GeneralProductManagement;
+using UniversalShopingClasses.MobileManagement;
+
+namespace UniversalShopingApp.Controllers
+{
+    public class MobileController : Controller
+    {
+        // GET: Mobile
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult CreateMobile()
+        {
+            DrinkHandler mHandler = new DrinkHandler();
+            ViewBag.Brands = ModelHelper.ToSelectItemList(mHandler.GetAllBrands());
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateMobile(Mobile mobile, FormCollection data)
+        {
+            UniversalContext db = new UniversalContext();
+            mobile.ProductBrand = new ProductBrand { Id = Convert.ToInt32(data["brand"]) };
+            mobile.Wifi = Convert.ToBoolean(data["wifi"].Split(',').First());
+            mobile.BlueTooth = Convert.ToBoolean(data["bluetooth"].Split(',').First());
+            mobile.ThreeG = Convert.ToBoolean(data["3g"].Split(',').First());
+            mobile.FourG = Convert.ToBoolean(data["4g"].Split(',').First());
+            long uno = DateTime.Now.Ticks;
+            int counter = 0;
+            foreach (string fcName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fcName];
+                if (!string.IsNullOrEmpty(file.FileName))
+                {
+                    string url = "/Content/ProductImages/" + uno + "_" + ++counter +
+                                 file.FileName.Substring(file.FileName.LastIndexOf("."));
+                    string path = Request.MapPath(url);
+                    //m.Images = file.FileName;
+                    file.SaveAs(path);
+                    mobile.ProductImages.Add(new ProductImages() { Url = url, Perority = counter });
+                }
+            }
+            using (db)
+            {
+                db.Mobiles.Add(mobile);
+                db.Entry(mobile.ProductBrand).State = EntityState.Unchanged;
+
+                db.SaveChanges();
+                return RedirectToAction("Success", "drink");
+            }
+
+        }
+    }
+}
