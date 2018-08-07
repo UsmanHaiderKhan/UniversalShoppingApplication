639222b7290b473007062add81c501b6f7312467
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-07 18:18:36 +0500

    Added New Classes which is commen foe all

---
+++ UniversalShopingClasses/CartManagement/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace UniversalShopingClasses.CartManagement
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string BuyerName { get; set; }
+        public string EmailAddress { get; set; }
+        public string FullAddress { get; set; }
+        public bool IsActive { get; set; }
+        public double Phone { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ UniversalShopingClasses/CartManagement/OrderHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace UniversalShopingClasses.CartManagement
+{
+    public class OrderHandler
+    {
+        public List<OrderDetail> GetAllOrders()
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.OrderDetails select c).ToList();
+            }
+        }
+
+        public List<Order> GetOrders()
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.Orders.Include(m => m.OrderDetails).Include(m => m.OrderStatus) select c).ToList();
+            }
+        }
+
+        public List<Order> GetOrdersDetails(int id)
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from s in db.Orders
+                        .Include(t => t.OrderDetails)
+                        where s.Id == id
+                        select s).ToList();
+            }
+        }
+
+        public OrderDetail GetOrderDetailById(int id)
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.OrderDetails where c.Id == id select c).FirstOrDefault();
+            }
+        }
+
+        public OrderStatus GetOrderStatusById(int id)
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.OrderStatuses where c.Id == id select c).FirstOrDefault();
+            }
+        }
+
+
+        public List<OrderDetail> GetAllBuyedDrink(int id)
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.OrderDetails where c.Id == id select c).ToList();
+            }
+        }
+    }
+}
