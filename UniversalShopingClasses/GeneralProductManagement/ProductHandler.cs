32338432b20dc63429454247f9b852dd6bb6a600
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-08 03:13:19 +0500

    Update MAke the Get Category and type Methods

--- UniversalShopingClasses/GeneralProductManagement/ProductHandler.cs
+++ UniversalShopingClasses/GeneralProductManagement/ProductHandler.cs
@@ -1,9 +1,72 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using UniversalShopingClasses.DrinksManagement;
+using UniversalShopingClasses.FabricsManagement;
+using UniversalShopingClasses.MobileManagement;
 
 namespace UniversalShopingClasses.GeneralProductManagement
 {
     public class ProductHandler
     {
+        public List<ProductType> GetAllTypes()
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.ProductTypes select c).ToList();
+            }
+
+        }
+
+        public ProductType GetProductTypeById(int id)
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.ProductTypes where c.Id == id select c).FirstOrDefault();
+            }
+        }
+
+        public List<Drink> GetDrinksType(int id)
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.Drinks
+                    .Include(m => m.ProductImages)
+                    .Include(m => m.ProductBrand)
+                        where c.ProductBrand.Id == id
+                        select c).ToList();
+            }
+        }
+
+        public List<Mobile> GetMobilesType(int id)
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.Mobiles
+                        .Include(m => m.ProductImages)
+                        .Include(m => m.ProductBrand)
+                        where c.ProductBrand.Id == id
+                        select c).ToList();
+            }
+        }
+
+        public List<Fabric> GetFabricssType(int id)
+        {
+            UniversalContext db = new UniversalContext();
+            using (db)
+            {
+                return (from c in db.Fabrics
+                        .Include(m => m.ProductImages)
+                        .Include(m => m.ProductBrand)
+                        where c.ProductBrand.Id == id
+                        select c).ToList();
+            }
+        }
+
         public object GetAllCategories()
         {
             UniversalContext db = new UniversalContext();
@@ -11,7 +74,6 @@ namespace UniversalShopingClasses.GeneralProductManagement
             {
                 return (from c in db.ProductBrands select c).ToList();
             }
-
         }
     }
 }
