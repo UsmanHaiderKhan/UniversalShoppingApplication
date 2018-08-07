a9a0a413ba8643ec71b41a67a52c833a21739596
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-08 03:16:26 +0500

    Added New Advertisement Class

---
+++ UniversalShopingClasses/OLXClass/Advertisement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using UniversalShopingClasses.GeneralProductManagement;
+using UniversalShopingClasses.UserManagement;
+
+namespace UniversalShopingClasses.OLXClass
+{
+    public class Advertisement
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar")]
+        [MaxLength(300)]
+        public string Title { get; set; }
+        [Column(TypeName = "varchar")]
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public DateTime PostedOn { get; set; }
+        public User User { get; set; }
+        public virtual AdvertisementSubCategory SubCategory { get; set; }
+        public virtual AdvertisementType Type { get; set; }
+        public virtual ICollection<ProductImages> Images { get; set; }
+
+    }
+}
