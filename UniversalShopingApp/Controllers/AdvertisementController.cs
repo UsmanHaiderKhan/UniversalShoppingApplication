397b75924cdd459bde95fd76eb203a489b93861a
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-08 03:08:48 +0500

    Update:Advertisement controller

---
+++ UniversalShopingApp/Controllers/AdvertisementController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace UniversalShopingApp.Controllers
+{
+    public class AdvertisementController : Controller
+    {
+        // GET: Advertisement
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult PostAdvertisement()
+        //{
+        //    ViewBag.Categories - new AdvertisemntHandler().GetCategories().
+        //    List<SelectListItem> tempList = new AdvertisemntHandler().GetTypes().ToSelectItemList();
+        //    tempList.First().Selected = true;
+        //    ViewBag.AdvTypes = tempList;
+        //}
+
+    }
+}
