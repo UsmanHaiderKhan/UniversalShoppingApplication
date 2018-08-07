70ce6f3b51072a49f4ba69072121fc1c87e2e665
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-08 03:18:59 +0500

    Update Added New AdvPostSummery

---
+++ UniversalShopingClasses/OLXClass/AdvSummeryModel.cs
@@ -0,0 +1,12 @@
+﻿namespace UniversalShopingClasses.OLXClass
+{
+    public class AdvSummeryModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
