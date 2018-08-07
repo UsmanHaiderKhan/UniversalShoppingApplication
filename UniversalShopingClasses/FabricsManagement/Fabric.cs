34d02fd6d9b7367fb4bfed8bab63da696da4a9f9
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-07 18:18:58 +0500

    Added New Class for NEw Product

---
+++ UniversalShopingClasses/FabricsManagement/Fabric.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UniversalShopingClasses.GeneralProductManagement;
+
+namespace UniversalShopingClasses.FabricsManagement
+{
+    public class Fabric
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public ICollection<ProductImages> ProductImages { get; set; }
+
+        public Fabric()
+        {
+            ProductImages = new List<ProductImages>();
+        }
+
+    }
+}
