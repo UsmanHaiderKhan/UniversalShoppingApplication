788ed628ecb1c92270f25db9e8b91fd6a472f34e
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-08 19:13:34 +0500

    Added New AlertMessage Class for Message

---
+++ UniversalShopingApp/Models/AlertMessage.cs
@@ -0,0 +1,50 @@
+﻿namespace UniversalShopingApp.Models
+{
+    public class AlertMessage
+    {
+        public AlertMessage() { }
+
+        public AlertMessage(string contents, AlertMessageType type)
+        {
+            switch (type)
+            {
+                case AlertMessageType.Error:
+                    cssClass = "alert-danger";
+                    heading = "Error!";
+                    break;
+                case AlertMessageType.Success:
+                    cssClass = "alert-success";
+                    heading = "Success!";
+                    break;
+                case AlertMessageType.Warning:
+                    cssClass = "alert-warning";
+                    heading = "Warning!";
+                    break;
+                default:
+                    cssClass = "alert-info";
+                    heading = "Information!";
+                    break;
+            }
+            this.contents = contents;
+        }
+
+
+        private string cssClass;
+        public string CSSClass { get { return cssClass; } }
+
+        private string heading;
+        public string Heading { get { return heading; } }
+
+        private string contents;
+        public string Content { get { return contents; } }
+    }
+
+    public enum AlertMessageType
+    {
+        Success,
+        Information,
+        Error,
+        Warning
+    }
+
+}
