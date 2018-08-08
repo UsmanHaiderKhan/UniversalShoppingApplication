a73bf67c6a3bbf62e84153a2a45ccdb48d4b3f55
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-08 19:13:59 +0500

    Added New Classs For Drop Down List

---
+++ UniversalShopingApp/Models/DDListView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace UniversalShopingApp.Models
+{
+    public class DDListView
+    {
+        public string Name { get; set; }
+
+        public string Caption { get; set; }
+
+        public IEnumerable<SelectListItem> Values { get; set; }
+
+        public string GlyphIcon { get; set; }
+    }
+}
