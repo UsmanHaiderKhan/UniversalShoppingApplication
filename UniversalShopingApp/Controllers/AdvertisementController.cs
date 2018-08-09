fb5f85dbddf8eeb8717f12d54fd7e2ef64071028
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-09 11:50:53 +0500

    Update :Admin Controller

--- UniversalShopingApp/Controllers/AdvertisementController.cs
+++ UniversalShopingApp/Controllers/AdvertisementController.cs
@@ -18,6 +18,7 @@ namespace UniversalShopingApp.Controllers
         {
             return View();
         }
+
         [HttpGet]
         public ActionResult PostAdvertisement()
         {
@@ -27,37 +28,50 @@ namespace UniversalShopingApp.Controllers
             ViewBag.AdvTypes = tempList;
             return View();
         }
+
         [HttpPost]
         public ActionResult PostAdvertisement(Advertisement advertisement, FormCollection data)
         {
-            advertisement.SubCategory = new AdvertisementSubCategory { Id = Convert.ToInt32(data["SubCategory"]) };
-            advertisement.Type = new AdvertisementType { Id = Convert.ToInt32(data["AdvType"]) };
-            advertisement.PostedOn = DateTime.Now;
-            long uno = DateTime.Now.Ticks;
-            int counter = 0;
-            foreach (string fcName in Request.Files)
+            try
             {
-                HttpPostedFileBase file = Request.Files[fcName];
-                if (!string.IsNullOrWhiteSpace(file.FileName))
+                advertisement.SubCategory = new AdvertisementSubCategory { Id = Convert.ToInt32(data["SubCategory"]) };
+                advertisement.Type = new AdvertisementType { Id = Convert.ToInt32(data["AdvType"]) };
+                advertisement.PostedOn = DateTime.Now;
+                long uno = DateTime.Now.Ticks;
+                int counter = 0;
+                foreach (string fcName in Request.Files)
                 {
-                    string ext = file.FileName.Substring(file.FileName.LastIndexOf("."));
-                    string url = "/Content/OLXImages/" + $"{uno}_{++counter}{ext}";
-                    string path = Request.MapPath(url);
-                    file.SaveAs(path);
-                    advertisement.Images.Add(new ProductImages { Url = url, Perority = counter });
+                    HttpPostedFileBase file = Request.Files[fcName];
+                    if (!string.IsNullOrWhiteSpace(file.FileName))
+                    {
+                        string ext = file.FileName.Substring(file.FileName.LastIndexOf("."));
+                        string url = "/Content/OLXImages/" + $"{uno}_{++counter}{ext}";
+                        string path = Request.MapPath(url);
+                        file.SaveAs(path);
+                        advertisement.Images.Add(new ProductImages { Url = url, Perority = counter });
+                    }
                 }
+                UniversalContext db = new UniversalContext();
+                using (db)
+                {
+                    db.Advertisements.Add(advertisement);
+                    db.Entry(advertisement.Type).State = EntityState.Unchanged;
+                    db.Entry(advertisement.SubCategory).State = EntityState.Unchanged;
+                    db.Entry(advertisement.User).State = EntityState.Unchanged;
+                    db.SaveChanges();
+                }
+                TempData.Add("Message", new AlertMessage("Advertisement added successfully", AlertMessageType.Success));
             }
-            UniversalContext db = new UniversalContext();
-            using (db)
+            catch (Exception ex)
             {
-                db.Advertisements.Add(advertisement);
-                db.Entry(advertisement.Type).State = EntityState.Unchanged;
-                db.Entry(advertisement.SubCategory).State = EntityState.Unchanged;
-                db.Entry(advertisement.User).State = EntityState.Unchanged;
-                db.SaveChanges();
-                return RedirectToAction("Success", "Drink");
+                TempData.Add("Message",
+                    new AlertMessage("Failed to add advertisements, try later", AlertMessageType.Error));
+                throw ex;
             }
+            return RedirectToAction("Success", "Drink");
+
         }
+
         [HttpGet]
         public ActionResult ByCategory(int id)
         {
@@ -71,7 +85,7 @@ namespace UniversalShopingApp.Controllers
             DDListView model = new DDListView();
             model.Name = "Category";
             model.Caption = "- Select Category -";
-            model.GlyphIcon = "glyphicon-tag";
+            model.GlyphIcon = "fa-tag";
             model.Values = new AdvertisemntHandler().GetCategories().ToSelectItemList();
             return PartialView("~/Views/Shared/_DDLView.cshtml", model);
         }
@@ -82,7 +96,7 @@ namespace UniversalShopingApp.Controllers
             DDListView model = new DDListView();
             model.Name = "SubCategory";
             model.Caption = "- Select Sub Category -";
-            model.GlyphIcon = "glyphicon-tag";
+            model.GlyphIcon = "fa-tag";
             model.Values = new AdvertisemntHandler().GetSubCategories(new AdvertismentCateory { Id = id }).ToSelectItemList();
             return PartialView("~/Views/Shared/_DDLView.cshtml", model);
         }
