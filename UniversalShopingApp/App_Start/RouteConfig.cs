01996d4f610783b421192249ceb7f9da0d76f474
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-10 03:29:39 +0500

    Update the Routes of the files

--- UniversalShopingApp/App_Start/RouteConfig.cs
+++ UniversalShopingApp/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace UniversalShopingApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "index", id = UrlParameter.Optional }
+                defaults: new { controller = "Contact", action = "FirstPage", id = UrlParameter.Optional }
             );
         }
     }
