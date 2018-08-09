b162f470f2a2fdb4a2c1ddb34bdf997adc170d1c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-10 01:54:37 +0500

    Added NEw Controller auction controller

---
+++ UniversalShopingApp/Controllers/AuctionController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace UniversalShopingApp.Controllers
+{
+    public class AuctionController : Controller
+    {
+        // GET: Auction
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
