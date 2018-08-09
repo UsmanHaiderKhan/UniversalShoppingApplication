d25681e4cdf01f3524e63bbef0f29339cc07427c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-10 02:03:50 +0500

    Update added new fabrichandler and added new action methods

---
+++ UniversalShopingClasses/FabricsManagement/FabricHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace UniversalShopingClasses.FabricsManagement
+{
+    public class FabricHandler
+    {
+        UniversalContext db = new UniversalContext();
+        public List<Fabric> GetAllProducts()
+        {
+            using (db)
+            {
+                return (from c in db.Fabrics
+                        .Include(m => m.ProductBrand)
+                        .Include(m => m.ProductImages)
+                        select c).ToList();
+            }
+        }
+
+        public Fabric GetFabricById(int id)
+        {
+            using (db)
+            {
+                return (from c in db.Fabrics
+                        .Include(m => m.ProductBrand)
+                        .Include(m => m.ProductImages)
+                        where c.Id == id
+                        select c).FirstOrDefault();
+            }
+        }
+
+    }
+}
