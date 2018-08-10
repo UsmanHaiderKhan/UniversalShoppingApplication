7e4db5e7a3d4f0cf5974be3329534bf237095f10
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-10 12:06:04 +0500

    Update Added NEw Class of Auction

---
+++ UniversalShopingClasses/AuctionManagement/Auction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UniversalShopingClasses.GeneralProductManagement;
+
+namespace UniversalShopingClasses.AuctionManagement
+{
+    public class Auction
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float BidPrice { get; set; }
+        public string Specification { get; set; }
+        public DateTime Postedon { get; set; }
+        public string Description { get; set; }
+        public AuctionCategory AuctionCategory { get; set; }
+        public ICollection<ProductImages> ProductImages { get; set; }
+
+        public Auction()
+        {
+            ProductImages = new List<ProductImages>();
+
+        }
+    }
+}
