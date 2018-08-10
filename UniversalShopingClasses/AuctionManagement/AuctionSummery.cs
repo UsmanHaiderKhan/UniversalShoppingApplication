053fc9db434158efce6e38b9af1cd096b66a0426
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-11 01:55:52 +0500

    Added NEw interface or summery list

---
+++ UniversalShopingClasses/AuctionManagement/AuctionSummery.cs
@@ -0,0 +1,11 @@
+﻿namespace UniversalShopingClasses.AuctionManagement
+{
+    public class AuctionSummery
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float BidPrice { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ UniversalShopingClasses/AuctionManagement/IBidable.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversalShopingClasses.AuctionManagement
+{
+    public interface IBidable
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+
+    }
+}
