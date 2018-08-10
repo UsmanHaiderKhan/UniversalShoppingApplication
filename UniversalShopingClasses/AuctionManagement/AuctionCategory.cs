5a21cffa16c69bb3e2509406f27436ee9b965a0d
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-10 12:06:20 +0500

    Added New Auctino Classes

---
+++ UniversalShopingClasses/AuctionManagement/AuctionCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace UniversalShopingClasses.AuctionManagement
+{
+    public class AuctionCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
