2588362e78e726fc4057288fb5d485b82b0f24e3
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-12 16:02:21 +0500

    Added New Controller For Chating

---
+++ UniversalShopingApp/Controllers/AuctionChatController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace UniversalShopingApp.Controllers
+{
+    public class AuctionChatController : Controller
+    {
+        // GET: AuctionChat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
