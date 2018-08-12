2d7571fce6d004da89611a5f242668f5d658b570
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-12 22:52:52 +0500

    Added New Class of FeedBack

---
+++ UniversalShopingClasses/AuctionManagement/FeedBack.cs
@@ -0,0 +1,10 @@
+﻿namespace UniversalShopingClasses.AuctionManagement
+{
+    public class FeedBack
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string FeedDetail { get; set; }
+
+    }
+}
