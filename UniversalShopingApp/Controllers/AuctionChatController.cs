77196d61bbc11622c4466cd8da2d629456c63d84
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-14 15:07:50 +0500

    Update the Auction Chat Controller After Adding the Functionalities

--- UniversalShopingApp/Controllers/AuctionChatController.cs
+++ UniversalShopingApp/Controllers/AuctionChatController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using UniversalShopingClasses.UserManagement;
 
 namespace UniversalShopingApp.Controllers
 {
@@ -7,6 +9,9 @@ namespace UniversalShopingApp.Controllers
         // GET: AuctionChat
         public ActionResult Index()
         {
+            User user = (User)Session[WebUtils.Current_User];
+            if (!(user != null && user.IsInRole(Convert.ToInt32(WebUtils.Current_User))))
+                return RedirectToAction("Login", "Users", new { ctl = "Login", act = "Users" });
             return View();
         }
 
