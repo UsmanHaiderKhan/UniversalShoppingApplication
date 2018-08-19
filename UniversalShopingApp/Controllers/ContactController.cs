b75e0aacdfd29bcc4deb22bd5fcdbab6a13cba8e
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-19 14:28:25 +0500

    Update remove the commented code in contact controller

--- UniversalShopingApp/Controllers/ContactController.cs
+++ UniversalShopingApp/Controllers/ContactController.cs
@@ -40,12 +40,6 @@ namespace UniversalShopingApp.Controllers
 
         public ActionResult FirstPage()
         {
-            //User user = (User)Session[WebUtils.Current_User];
-            //if (user == null)
-            //{
-            //    return RedirectToAction("Login", "Users");
-            //}
-
             return View();
         }
 
